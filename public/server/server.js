dadd8f21b05dc75066a30dbc68c84f58ed34e43d
Markova Darya <markova@reksoft.ru>
2017-11-07 17:48:40 +0300

    использование сервиса socket.io

--- public/server/server.js
+++ public/server/server.js
@@ -1,20 +1,52 @@
-var http = require('http');
-var server = http.createServer();
-var socket_io = require('socket.io');
-//https://github.com/itaylor/redux-socket.io
-server.listen(3003);
-var io = socket_io();
-io.attach(server);
-io.on('connection', function(socket) {
-  console.log("Socket connected: " + socket.id);
+const io = require('socket.io')();
 
-  //отлавливаем действия пользователя
-  socket.on('action', (action) => {
-    console.log('SERVER CATCH ACTION', action.type);
-    //socket.emit('action', action);
-    /*if (action.type === 'server/new-message') {
-      console.log('Got new message', action.message);
-      socket.emit('action', {type: 'response', data: "I've receive message"});
-    }*/
+let messages = [];//буфер сообщений
+
+const size = 10; //размер буфера
+
+io.on('connection', (socket) => {
+  //сообщаем о подключении
+  socket.emit('server-connect');
+
+  if (messages.length > 0) {
+    //только что подключившемуся клиенту отправляем накопленные сообщения из буфера
+    for(let index in messages) {
+      let savedMessage = messages[index];
+      let sendMessage = {text: savedMessage.text, whose: savedMessage.clientId === socket.id};
+      socket.emit('new-message', sendMessage);
+    }
+  }
+
+  socket.on('send-message', (message) => {
+    saveMessageToBuffer(message, socket.id);
+
+    const clients = io.sockets.sockets;
+    let newMessage = {};
+
+    //рассылаем сообщение всем клиентам
+    Object.keys(clients).forEach((id) => {
+      newMessage.text = message;
+      newMessage.whose = id === socket.id;
+      clients[id].emit('new-message', newMessage);
+    });
+
+    //io.sockets.emit('new-message', newMessage);
+    //обрезаем буфер сообщений до  размера size, сохраняя последние сообщения
+    if (messages.length >= size) {
+      messages = messages.slice(-1*size);
+    }
+  });
+
+  //server-disconnect
+  socket.on('disconnect', (reason) => {
+    socket.emit('server-disconnect', reason);
   });
 });
+
+saveMessageToBuffer = (msgText, socketId) => {
+  messages.push({text:msgText, clientId: socketId});
+};
+const port = 3003;
+io.listen(port);
+
+console.log('listening on port ', port);
