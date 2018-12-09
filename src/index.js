196883e94572d18b0c973a433dfb103f0cf8a2cc
darya-markova <darya.towarddreams@gmail.com>
2018-12-09 22:34:24 +0300

    рефакторинг компоннента Chat

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import thunk from 'redux-thunk';
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import thunk from 'redux-thunk';
 import { subscribeChatEvents } from './reducers/ChatEventsReducer';
 
 
